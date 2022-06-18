becac61f2455e828de99dda631dda7d667b0417a
mercy olekanma okorie <okorie.olekanma@gmail.com>
2022-06-18 12:11:13 +0100

    unable to create a virtual environment with .env. using another name for the folder worked after several attempts.

---
+++ task/studentid14G003030QOM/blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+# Create your models here.
+class post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_date = models.DateTimeField()
+    published_date = models.DateTimeField()
